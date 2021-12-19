1b3130f305570289801fa768069460739c6ceb0c
redpeacock78 <scarletpeacock78@gmail.com>
2021-12-20 03:50:18 +0900

    chore: Remove jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  coverageProvider: 'v8',
-  transform: {
-    '^.+\\.ls$': 'livescript-jest',
-    '^.+\\.(j|t)sx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ls', 'ts', 'tsx'],
-};
